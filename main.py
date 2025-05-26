9938d816951896aa24468d2ff51682d56c286daa
aniketjaldu <twitchxtr3amer@gmail.com>
2025-05-26 11:54:30 -0400

    Updated typo "12" to "10"

--- main.py
+++ main.py
@@ -71,7 +71,8 @@ async def rectangle_area(rect: Rectangle):
 async def power(base: int, exp: int = 2):
     return {"base": base, "exp": exp, "result": base ** exp}
 
-# 12. GET list: /colors
+# 10. GET list: /colors
 @app.get("/colors")
 async def list_colors():
     return {"colors": ["red", "blue", "green", "yellow"]}
+
